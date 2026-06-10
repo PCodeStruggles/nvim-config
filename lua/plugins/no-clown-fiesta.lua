bde1d082cb1ecb7cbfbf8fecf791ed7e12c82e97
Paolo Arnaudo <arnaudo.paolo20@gmail.com>
2026-06-10 08:12:47 +0200

    Included no-clown-fiesta as main theme

---
+++ lua/plugins/no-clown-fiesta.lua
@@ -0,0 +1,20 @@
+return {
+  "aktersnurra/no-clown-fiesta.nvim",
+  lazy = false,
+  priority = 1000,
+  config = function()
+    require("no-clown-fiesta").setup({
+      transparent = false,
+      styles = {
+        -- Force absolute flatness across the board
+        comments = { italic = false },
+        keywords = { bold = false },
+        functions = { bold = false },
+        variables = {},
+        type = { bold = false },
+        lsp = { underline = false },
+      },
+    })
+    vim.cmd([[colorscheme no-clown-fiesta]])
+  end
+}
